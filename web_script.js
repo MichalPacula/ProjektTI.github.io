21545c74d20c8b99a5db162e5d556059eb646b26
DuuzOrk <michal.pacula60@gmail.com>
2023-01-13 15:31:22 +0100

    Changed nav bar once again, added login panel.

--- web_script.js
+++ web_script.js
@@ -12,6 +12,26 @@ function closePopup(popupChosen){
     popup.style.transform = "translate(-50%, -50%) scale(0.1)";
 }
 
+window.onload = function (){
+    let sign_up_button = document.getElementById("sign_up_button");
+    let sign_in_button = document.getElementById("sign_in_button");
+    let name_field = document.getElementById("name_field");
+    let title = document.getElementById("title");
+    sign_in_button.onclick = function (){
+        name_field.style.maxHeight = "0";
+        title.innerHTML = "Sign In";
+        sign_up_button.classList.add("disable");
+        sign_in_button.classList.remove("disable");
+    }
+    sign_up_button.onclick = function (){
+        name_field.style.maxHeight = "60px";
+        title.innerHTML = "Sign Up";
+        sign_up_button.classList.remove("disable");
+        sign_in_button.classList.add("disable");
+    }
+}
+
+
 var count = 2;
 function automaticSlider(){
     var currentUrl = document.URL;
